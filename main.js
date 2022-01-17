365ebf9e7d5b648a5bc115b2666b37259a565e61
Albina2611 <91902727+Albina2611@users.noreply.github.com>
2022-01-17 19:54:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,474 @@
+// let a = prompt('Введіть значення сторони а')
+// let p = a*4
+
+// alert (p)
+
+
+
+// let a = prompt ('take number')
+// let s = a*a
+
+// alert(s)
+
+
+
+// let a = prompt('take number one')
+// let b = prompt('take number two')
+// let s = a*b
+// let p = 2*a+2*b
+
+// alert(`S=${s}  P=${p}`)
+
+
+
+
+// let d = prompt('take "d"')
+// let п = 3.14
+// let l = d*п
+
+// alert(l)
+
+
+
+
+// let a = prompt('введіть довжину ребра куба')
+// let v = a*a*a
+// let s = 6*a*a
+
+// alert(`V=${v}   S=${s}`)
+
+
+
+
+// let a = prompt('take "a"')
+// let b = prompt('take "b"')
+// let c = prompt('take "c"')
+// let v = a*b*c
+// let s = 2*a*b+2*b*c+2*a*c
+
+// alert(`V=${v}   S=${s}`)
+
+
+
+
+// let r = prompt('take "R"')
+// let p = 3.14
+// let L = 2*p*r
+// let s = p*r*r
+
+// alert(`L=${L}   S=${s}`)
+
+
+
+// let a = prompt('take number one')
+// let b = prompt('take number two')
+// let k = a/2+b/2
+
+
+// alert(`Середнє арифметичне=${k}`)
+
+
+
+
+// let a = prompt('take number one')
+// let b = prompt('take number two')
+// let d = Math.sqrt(a*b)
+
+// alert(d)
+
+
+
+
+// let a = prompt('null number')
+// let b = prompt('null number')
+// let s = a*b
+// let e = a*1 + b*1
+// let c = Math.pow(a , 2)/Math.pow(b , 2)
+// let w = a-b
+
+// alert(`Сума: ${e}   Різниця: ${w}   Добуток: ${s}   Ділення іх модулей: ${c}`)
+
+
+
+
+// let a = prompt('take number a')
+// let b = prompt('take number b')
+// let e = a*1+b*1
+// let r = a-b
+// let m = a*b
+// let c = Math.abs (a)/Math.abs (b)
+
+// alert(e + ' ' + r + ' ' + m + ' ' +c)
+
+
+
+
+// let a = prompt('take number "a"')
+// let b = prompt('take number "b"')
+// let c = Math.hypot(a , b)
+// let p = a*1+b*1+c*1
+
+// alert(`C=${c}    P=${p}`)
+
+
+
+
+// let r1 = prompt('take r1, r1>r2')
+// let r2 = prompt('take r2, r2<r1')
+// let p = 3.14
+// let s1 = p*Math.pow(r1,2)
+// let s2 = p*Math.pow(r2,2)
+// let s3 = s1-s2
+
+// alert(s1 + '  ' + s2 + '  ' + s3)
+
+
+
+
+// let l = prompt('take "l"')
+// let p = 3.14
+// let r = l*1/(2*p)
+// let s = p*r*r
+
+// alert(r+'   '+s)
+
+
+
+
+// let p = 3.14
+// let s = prompt('take "S"')
+// let d = Math.sqrt(4*s/p)
+// let l = p*d
+
+// alert(d+'   '+l)
+
+
+
+
+// let x1 = prompt('take "x1"')
+// let x2 = prompt('take "x2"')
+// let d = Math.abs(x2*1 - x1*1)
+
+// alert(d)
+
+
+
+
+// let a = prompt('A=')
+// let b = prompt('B=')
+// let c = prompt('C=')
+// let s1 = a*1+c*1
+// let s2 = b*1+c*1
+// let sk = s1*1+s2*1
+
+// alert(s1+' '+s2+' '+sk)
+
+
+
+
+// let a = prompt('A=')
+// let b = prompt('B=')
+// let ab = a*1 + b*1
+// let c = ab*1/2
+// let ac = c*1+a*1
+// let bc = b*1+c*1
+// let s  = ac*1+bc*1
+
+// alert(ac+'  '+bc+'  '+s)
+
+
+
+
+// let x2 = prompt('x2=')
+// let x1 = prompt('x1=')
+// let y2 = prompt('y2=')
+// let t1 = prompt('y1=')
+// let s = x2*1-x1*1
+// let s0 = y2*1-t1*1
+// let s1 = s*1+s0*1
+// let s2 = Math.sqrt(s1)
+
+// alert(s2)
+
+
+
+
+// let x1 = prompt('x1=')
+// let y1 = prompt('y1=')
+// let x2 = prompt('x2=')
+// let y2 = prompt('y2=')
+// let kk = Math.sqrt((x2*1-x1*1)+(y2*1-y1*1))
+// let S = Math.pow(kk,2)
+// let P = kk*4
+
+// alert(S+'   '+P)
+
+
+
+
+// let l = prompt('L=')
+// let m = 100
+
+// alert(Math.floor(l/m))
+
+
+
+
+// let m = prompt('M=')
+// let t = 1000
+
+// alert(Math.floor(m/t))
+
+
+
+
+// let bt = prompt('take bt')
+// let kb = 1024
+
+// alert(Math.floor(bt/kb))
+
+
+
+
+// let a = prompt('take A, A>B')
+// let b = prompt('take b, B<A')
+
+// alert(Math.floor(a/b)
+
+
+
+
+// let a = prompt('take A, A>B')
+// let b = prompt('take B, B<A')
+// let o = a%b
+
+// alert(o)
+
+
+
+
+// let ab = prompt('take number ab')
+// let a = Math.floor(ab/10)
+// let b = ab%10
+
+// alert(a+'  '+b)
+
+
+
+
+// let ab = prompt('take ab')
+// let a  = Math.floor(ab/10)
+// let b = ab%10
+// let sum = a*1+b*1
+// let mn = a*b
+
+// alert(sum+'  '+mn)
+
+
+
+
+// let ab = prompt('take ab')
+// let a  = Math.floor(ab/10)
+// let b = ab%10
+
+// alert(`${b}${a}`)
+
+
+
+
+// let abc = prompt('take number "abc"')
+// let c = Math.floor(abc/100)
+
+// alert(c)
+
+
+
+
+// let abc = prompt('take number "abc"')
+// let a = abc%10
+// let b = Math.floor((abc/ 10) % 10)
+
+// alert(a+'  '+b)
+
+
+
+
+// let abc = prompt('take number"abc"')
+// let a = abc%10
+// let b = Math.floor((abc/ 10) % 10)
+// let c = Math.floor(abc/ 100)
+// let s = a*1+b*1+c*1
+// let m = a*b*c
+
+// alert(s+'   '+m)
+
+
+
+
+// let abc = prompt('take number "abc"')
+// let a = abc%10
+// let b = Math.floor((abc/ 10) % 10)
+// let c = Math.floor(abc/ 100)
+
+// alert(`${a}${b}${c}`)
+
+
+
+
+// let abc = prompt('take number "abc"')
+// let a = abc%10
+// let b = Math.floor((abc/ 10) % 10)
+// let c = Math.floor(abc/ 100)
+
+// alert(`${b}${c}${a}`)
+
+
+
+
+// let cba = prompt('take number "abc"')
+// let a = cba%10
+// let b = Math.floor((cba/ 10) % 10)
+// let c = Math.floor(cba/ 100)
+
+// alert(`${a}${c}${b}`)
+
+
+
+
+// let abc = prompt('take number "abc"')
+// let c = abc%10
+// let b = Math.floor((abc/ 10) % 10)
+// let a = Math.floor(abc/ 100)
+
+// alert(`${b}${a}${c}`)
+
+
+
+
+
+// let abc = prompt('take number "abc"')
+// let c = abc%10
+// let b = Math.floor((abc/ 10) % 10)
+// let a = Math.floor(abc/ 100)
+
+// alert(`${a}${c}${b}`)
+
+
+
+
+// let h = prompt('take number,   abc>999')
+// let a = Math.floor(h/ 1000)
+// let b = Math.floor((h/ 100) % 10)
+
+// alert(`${a}    ${b}`)
+
+
+
+
+// let h = prompt('take number "abcd",   abcd>999')
+// let a = Math.floor(h/ 1000)
+
+// alert(a)
+
+
+
+
+// let n = prompt('take number, namber is cile')
+// let m = Math.floor(n/60)
+
+// alert(`Min=${m}`)
+
+
+
+
+// let n = prompt('take number, cile')
+// let ch = Math.floor(n/3600)
+
+// alert(ch)
+
+
+
+
+// let n = prompt('take number, secund')
+// let ch = n%60
+
+// alert(ch)
+
+
+
+
+// let n = prompt('take number, secund')
+// let ch = n%3600
+
+// alert(ch)
+
+
+
+
+// let n = prompt('take number, min')
+// let ch = n%60
+
+// alert(ch)
+
+
+
+
+// let k = prompt('1 < number < 365')
+// let n = k%7
+
+// alert(n)
+
+
+
+
+// let k = prompt('1 < number < 365')
+// let n = k%7+3
+
+// alert(n)
+
+
+
+
+// let k = prompt('1 < number < 365')
+// let n = k%7+1
+
+// alert(n)
+
+
+
+
+// let k = prompt('1 < number < 365')
+// let n = k%7+5
+
+// alert(n)
+
+
+
+
+// let k = prompt('take number one, 1-365')
+// let n = prompt('take number two, 1-7')
+// let nd = k%7+n*1
+
+// alert(nd)
+
+
+
+
+// let a = prompt('A=')
+// let b = prompt('B=')
+// let c = prompt('C=') 
+// let ab = a*b
+// let cc = c**2
+// let k = Math.floor(ab/cc)
+// let mn = ab*1-k*cc
+
+// alert(`${k}    ${mn}`)
+
+
+
+
+// let a = prompt('take number XXXX')
+// let b = Math.floor(a/100)
+// let st = b*1+1
+
+// alert(st)
